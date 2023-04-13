e2a4317b43d2982480aca87e9f54c8facd3d2ea7
CSmith3723 <123496273+CSmith3723@users.noreply.github.com>
2023-04-13 18:41:47 -0400

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -2,6 +2,8 @@ package application;
 
 import java.lang.String;
 import java.util.Collections;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
@@ -10,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
@@ -25,66 +28,145 @@ public class GUI extends Application{
 	launch(args);
 	}
 	
-	
+	ObservableList<String> list;
+	GridPane grid = new GridPane();
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		primaryStage.setTitle("Cost of Living Calculator");
 		
+ 		grid = createGridPane();
+ 		Button calculate = createCalculateButton();	
+ 		createCloseButton();
+ 		ImageView bayAreaMap = createNewMapZone();
+ 		ChoiceBox<String> areaList = createAreaList();		
 		final String mainStyle = "design.css";
+
+
+		//initializing scene
+		Scene scene = new Scene(grid, 1100, 700);
+		scene.getStylesheets().add(mainStyle);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+					
 		
-		//setting GridPane
-		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(10,10,10,10));
-		grid.setVgap(8);
-		grid.setHgap(10);
-		
-		//User input labels
-		Label hourlyWage = new Label("Hourly Wage*:");
-		Label expenses = new Label("Monthly Expenses*:");
-		Label areaLabel = new Label("Area:");		
-		Label hours = new Label("Avg. hours worked*:");
-		Label requiredInput = new Label("*required information");
-		//Setting output labels
-		
-		Label wageTextLabel = new Label("Monthly Wages:");
-		Label monthlyExpenseLabel = new Label("Monthly Expenses:");
+		//These labels are needed for button action, so cannot be set in a separate method. 
 		Label expenseOutputLabel = new Label();
-		Label grossWageOutput = new Label();
-		Label areaOutputText = new Label("Area:");
 		Label areaOutputLabel = new Label();
-		Label incomeTextLabel = new Label("Net Income:");
-		Label netWageOutput = new Label();
-		Label oneBRAppPriceText = new Label("Median price for 1 BR Appt:");
-		Label twoBRAppPriceText = new Label("Median price for 2 BR Appt:");
-		Label housePriceText = new Label("Median price for house:");
 		Label oneBRAppOutput = new Label();
+		Label oneBRAfford = new Label();
 		Label twoBRAppOutput = new Label();
+		Label twoBRAfford = new Label();
 		Label housePriceOutput = new Label();
+		Label grossWageOutput = new Label();
+		Label netWageOutput = new Label();
 		
-		//button to calculate inputs
-		Button calculate = new Button("Calculate");	
-			
-		//button to close program
-		Button close = new Button("Close");
-		close.setOnAction(e -> {
-			Platform.exit();
-		});
-		
+		GridPane.setConstraints(expenseOutputLabel, 4, 7);
+		GridPane.setConstraints(areaOutputLabel, 4, 9);	
+		GridPane.setConstraints(oneBRAppOutput, 3, 13);
+		GridPane.setConstraints(oneBRAfford, 3, 14);
+		GridPane.setConstraints(twoBRAppOutput, 3, 16);
+		GridPane.setConstraints(twoBRAfford, 3, 17);
+		GridPane.setConstraints(housePriceOutput, 3, 19);
+		GridPane.setConstraints(grossWageOutput, 4, 6);
+		GridPane.setConstraints(netWageOutput, 4, 8);
+
+		oneBRAfford.getStyleClass().add("bold");
+		twoBRAfford.getStyleClass().add("bold");
 		
 		//information input fields
 		TextField wageInput = new TextField();
 		wageInput.setPromptText("$/hr.");
+		GridPane.setConstraints(wageInput, 1, 0);
+		
 		TextField expenseInput = new TextField();
 		expenseInput.setPromptText("Expenses in $");
+		GridPane.setConstraints(expenseInput, 3, 0);
+		
 		TextField avgHours = new TextField();
 		avgHours.setPromptText("Average hrs./week");
+		GridPane.setConstraints(avgHours, 3, 4);
 		
-		//area ChoiceBox
-		ChoiceBox<String> areaChoice = new ChoiceBox<>();
+		//adding elements to grid pane layout
+		grid.getChildren().addAll(wageInput, oneBRAppOutput, grossWageOutput, oneBRAfford, twoBRAppOutput, 
+				twoBRAfford, netWageOutput, housePriceOutput,expenseInput, avgHours, bayAreaMap, 
+				expenseOutputLabel, areaOutputLabel);
+				
 		
-		//Create observable list to sort choice box entries		
-		ObservableList<String> list = FXCollections.observableArrayList("Apollo Beach", "Brandon", "Carrollwood", 
+		
+		//Action handler for button to take user input, calculate information, and output it to labels
+		calculate.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			
+			
+			public void handle(ActionEvent event) {
+				try {
+					
+			isDouble(avgHours.getText(), wageInput.getText(), expenseInput.getText());
+						
+			grossWageOutput.setText("$" + grossMonthlyWage(avgHours.getText(), wageInput.getText()));
+			
+			netWageOutput.setText("$" + netMonthlyWage(wageInput.getText(), expenseInput.getText(), avgHours.getText()));
+			
+				}
+					catch(NumberFormatException e){
+				}
+			
+				
+			//receive choice box input and set as string in output label
+			getChoice(areaList);		
+			String areaLabel = areaList.getValue();
+			areaOutputLabel.setText(areaLabel);
+			
+			//receive expense input and set as string in output label
+			String expenseOutput = expenseInput.getText();
+			expenseOutputLabel.setText("$ " + expenseOutput);
+			
+			//create map object from custom maps class to display map image for chosen area
+			Maps obj = new Maps();
+			bayAreaMap.setImage(obj.displayMap(areaList));
+			
+			//get value of choice box as string, get index value of string, 
+			//create HousingPrices obj, pass index value to method to retrieve price
+			String areaChoiceInput = areaList.getValue();
+			
+			int areaChoiceValue = list.indexOf(areaChoiceInput);
+			
+			HousingPrices prices = new HousingPrices();
+			oneBRAppOutput.setText("$ " + prices.get1BrAppt(areaChoiceValue));
+			twoBRAppOutput.setText("$ " + prices.get2BrAppt(areaChoiceValue));
+			housePriceOutput.setText("$ " + prices.getHouse(areaChoiceValue));
+			
+			String netIncome = netMonthlyWage(wageInput.getText(), expenseInput.getText(), avgHours.getText());
+
+			oneBRAfford.setText(prices.affordability1BR(areaChoiceValue, netIncome));
+			twoBRAfford.setText(prices.affordability2BR(areaChoiceValue, netIncome));
+			
+			
+			}});
+		
+		
+		
+		//**********Style Elements*********//
+		
+		
+		
+
+		
+		
+	}
+	
+
+	
+	//*****************************Methods***************************//
+
+
+
+	private ChoiceBox<String> createAreaList() {
+ChoiceBox<String> areaChoice = new ChoiceBox<>();
+		
+		///Create observable list to sort choice box entries		
+		list = FXCollections.observableArrayList("Apollo Beach", "Brandon", "Carrollwood", 
 				"Lutz", "Northdale", "Oldsmar", 
 				"Westchase", "Wesley Chapel", "Plant City", 
 				"Thonotosassa", "Town & Country", "Riverview", "Citrus Park", 
@@ -92,150 +174,187 @@ public class GUI extends Application{
 		Collections.sort(list);
 		areaChoice.setItems(list);
 		
-		
-		
-		//set base image for page, set constraints
-		Image mapTampa = new Image("Base_Tampa_Google_Map.png");
-		ImageView pic = new ImageView();
-		pic.setImage(mapTampa);
-		pic.setFitHeight(400);
-		pic.setFitWidth(400);
-		GridPane.setConstraints(pic, 0 ,8);
-		GridPane.setRowSpan(pic, 13);
-		GridPane.setColumnSpan(pic, 4);
-		
-		
-		//Action handler for taking user input and outputting it to labels
-		calculate.setOnAction(new EventHandler<ActionEvent>(){
-			@Override
-			public void handle(ActionEvent event) {
-				try {
-			
-			//validate input is double, get double as string
-			isDouble(avgHours,avgHours.getText());
-			isDouble(wageInput, wageInput.getText());
-			isDouble(expenseInput, expenseInput.getText());
-			
-			//output received text into label 
-			grossWageOutput.setText(grossMonthlyWage(wageInput.getText(), avgHours.getText()));
-			netWageOutput.setText(netMonthlyWage(wageInput.getText(), expenseInput.getText(), avgHours.getText()));
-				}
-					catch(NumberFormatException e){
-				}
-			
-			//receive choice box input and set as string in output label
-			getChoice(areaChoice);		
-			String areaLabel = areaChoice.getValue();
-			areaOutputLabel.setText(areaLabel);
-			
-			//receive expense input and set as string in output label
-			String expenseOutput = expenseInput.getText();
-			expenseOutputLabel.setText(expenseOutput);
-			
-			//create map object from custom maps class to display map image for chosen area
-			Maps obj = new Maps();
-			pic.setImage(obj.displayMap(areaChoice));
-			
-			//get value of choice box as string, get index value of string, 
-			//create HousingPrices obj, pass index value to method to retrieve price
-			String areaChoiceInput = areaChoice.getValue();
-			int areaChoiceValue = list.indexOf(areaChoiceInput);
-			HousingPrices prices = new HousingPrices();
-			oneBRAppOutput.setText(prices.get1BrAppt(areaChoiceValue));
-			twoBRAppOutput.setText(prices.get2BrAppt(areaChoiceValue));
-			housePriceOutput.setText(prices.getHouse(areaChoiceValue));
-			}});
-		
-		
-		//grid pane constraints
-		GridPane.setConstraints(wageInput, 1, 0);
-		GridPane.setConstraints(expenseInput, 3, 0);
 		GridPane.setConstraints(areaChoice, 1, 4);
-		GridPane.setConstraints(hours, 2, 4);
+		grid.getChildren().add(areaChoice);
+		areaChoice.getStyleClass().add("glass-blue");
 
-		//Grid Pane constraints for output labels
-		GridPane.setConstraints(hourlyWage, 0, 0);
-		GridPane.setConstraints(expenses, 2, 0);
-		GridPane.setConstraints(areaLabel, 0, 4);
-		GridPane.setConstraints(requiredInput, 2, 5);
-		GridPane.setConstraints(avgHours, 3, 4);
-		GridPane.setConstraints(wageTextLabel, 3, 6);
-		GridPane.setConstraints(grossWageOutput, 4, 6);
-		GridPane.setConstraints(netWageOutput, 4, 8);
-		GridPane.setConstraints(incomeTextLabel, 3, 8);
-		GridPane.setConstraints(expenseOutputLabel, 4, 7);
-		GridPane.setConstraints(monthlyExpenseLabel, 3, 7);
-		GridPane.setConstraints(areaOutputText, 3, 9);
-		GridPane.setConstraints(areaOutputLabel, 4, 9);		
-		GridPane.setConstraints(oneBRAppPriceText, 3,12 );	
-		GridPane.setConstraints(oneBRAppOutput, 3, 13);
-		GridPane.setConstraints(twoBRAppPriceText, 3, 14);
-		GridPane.setConstraints(twoBRAppOutput, 3, 15);
-		GridPane.setConstraints(housePriceText, 3, 16);
-		GridPane.setConstraints(housePriceOutput, 3, 17);
-				
+		return areaChoice;
 		
-		//grid constraints for buttons
+	}
+
+
+	private ImageView createNewMapZone() {
+		//set base image for page, set constraints
+				Image mapTampa = new Image("Base_Tampa_Google_Map.png");
+				ImageView pic = new ImageView();
+				pic.setImage(mapTampa);
+				pic.setFitHeight(400);
+				pic.setFitWidth(400);
+				GridPane.setConstraints(pic, 0 ,8);
+				GridPane.setRowSpan(pic, 13);
+				GridPane.setColumnSpan(pic, 4);
+				return pic;
+	}
+
+
+
+	private Button createCalculateButton() {
+		Button calculate = new Button("Calculate");
 		GridPane.setConstraints(calculate, 2, 6);
-		GridPane.setConstraints(close, 4, 0);
+		grid.getChildren().add(calculate);
+		calculate.getStyleClass().addAll("bold", "glass-blue");
 		
-		//adding elements to grid pane layout
-		grid.getChildren().addAll(hourlyWage, wageInput, expenses, expenseInput,areaLabel,areaChoice, calculate, 
-				grossWageOutput, expenseOutputLabel, wageTextLabel, monthlyExpenseLabel, areaOutputText, 
-				hours, avgHours, incomeTextLabel, areaOutputLabel, netWageOutput, pic, oneBRAppPriceText, 
-				twoBRAppPriceText, housePriceText, oneBRAppOutput, twoBRAppOutput, housePriceOutput, requiredInput, close);
-		
-		//initializing scene
-		Scene scene = new Scene(grid, 1000, 650);
-		scene.getStylesheets().add(mainStyle);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		return calculate;
 	}
 	
 	
-	
-	
+	private Button createCloseButton() {
+		Button close = new Button("Close");
+		GridPane.setConstraints(close, 4, 0);
+		grid.getChildren().add(close);
+		close.getStyleClass().addAll("bold", "glass-blue");
+		close.setOnAction(e -> {
+				Platform.exit();
+			});
+		
+		return close;
+	}
+
+
+
 	//method to validate input is double for wages and expenses
-	private boolean isDouble(TextField input, String message) {
+	private boolean isDouble(String avgHours, String wageInput, String expenseInput) {
 		try {
-			double amount = Double.parseDouble(message);
-			System.out.println(amount);
+			Double.parseDouble(avgHours);
+			Double.parseDouble(wageInput);
+			Double.parseDouble(expenseInput);
+			
 			return true;
 		}catch(NumberFormatException e){
 			AlertBox.display("Number Error","Please enter a valid number.");
 			return false;
 		}
+		
+		
 	}
 	
 	//method to retrieve user choice from choice box menu
 	private void getChoice(ChoiceBox<String> areaChoice) {
-		String input =  areaChoice.getValue();
-		System.out.println(input);
+		areaChoice.getValue();
+		
 	}
 	
+	
+	
 	private String grossMonthlyWage(String wageInput, String avgHours) {
+		
 		double wage = Double.parseDouble(wageInput);
 		double hours = Double.parseDouble(avgHours);
 		
 		double monthlyWage = (wage * hours) * 4;
+		
 		String grossMonthlyWage = String.valueOf(monthlyWage);
-		return grossMonthlyWage;
+		
+		return grossMonthlyWage;		
 	}
 	
+	
 	private String netMonthlyWage(String wageInput, String expenseInput, String avgHours) {
+				
 		double wage = Double.parseDouble(wageInput);
 		double hours = Double.parseDouble(avgHours);
+		double costs = Double.parseDouble(expenseInput);
+
+		
 		double weeklyWage = wage * hours; 
 		double monthlyWage = weeklyWage * 4;
-		double costs = Double.parseDouble(expenseInput);
-		
 		double netMonthly = monthlyWage - costs;
+		
 		String netMonthlyOutput = String.valueOf(netMonthly);
+				
 		return netMonthlyOutput;
 	}
 	
+	
+	private GridPane createGridPane() {
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10,10,10,10));
+		grid.setVgap(10);
+		grid.setHgap(10);
+		
+//		Button calculate = createCalculateButton();				
+ 		Button close = createCloseButton();
+ 		ChoiceBox<String> areaList = createAreaList();		
+		
+		Label hourlyWage = new Label("Hourly Wage*:");
+		Label expenses = new Label("Monthly Expenses*:");
+		Label areaLabel = new Label("Area:");		
+		Label hours = new Label("Avg. weekly hours*:");
+		Label requiredInput = new Label("*required information");
+		Label oneBRAppPriceText = new Label("Median price for 1 BR Appt:");		
+		Label monthlyExpenseText = new Label("Monthly Expenses:");
+		Label areaOutputText = new Label("Area:");		
+		Label netWageText = new Label("Net Income:");		
+		Label wageTextLabel = new Label("Monthly Wages:");
+		Label housePriceText = new Label("Median price for house:");
+		Label twoBRAppPriceText = new Label("Median price for 2 BR Appt:");	
+		
+		Label twoBRAppOutput = new Label();
+		Label twoBRAfford = new Label();
+		Label housePriceOutput = new Label();	
+		Label oneBRAfford = new Label();
+		Label oneBRAppOutput = new Label();
+		Label netWageOutput = new Label();
+		Label areaOutputLabel = new Label();		
+		Label expenseOutputLabel = new Label();
+		
+		GridPane.setConstraints(expenseOutputLabel, 4, 7);
+		GridPane.setConstraints(areaOutputLabel, 4, 9);	
+		GridPane.setConstraints(oneBRAppOutput, 3, 13);
+		GridPane.setConstraints(oneBRAfford, 3, 14);
+		GridPane.setConstraints(twoBRAppPriceText, 3, 15);
+		GridPane.setConstraints(twoBRAppOutput, 3, 16);
+		GridPane.setConstraints(twoBRAfford, 3, 17);
+		GridPane.setConstraints(housePriceText, 3, 18);
+		GridPane.setConstraints(housePriceOutput, 3, 19);
+		GridPane.setConstraints(wageTextLabel, 3, 6);
+		GridPane.setConstraints(hours, 2, 4);
+		GridPane.setConstraints(hourlyWage, 0, 0);
+		GridPane.setConstraints(expenses, 2, 0);
+		GridPane.setConstraints(areaLabel, 0, 4);
+		GridPane.setConstraints(requiredInput, 2, 5);
+		GridPane.setConstraints(expenseOutputLabel, 4, 7);
+		GridPane.setConstraints(monthlyExpenseText, 3, 7);
+		GridPane.setConstraints(areaOutputText, 3, 9);
+		GridPane.setConstraints(areaOutputLabel, 4, 9);	
+		GridPane.setConstraints(oneBRAppPriceText, 3,12 );	
+		GridPane.setConstraints(netWageText, 3, 8);
+		
+		netWageText.getStyleClass().add("underline");	
+		hourlyWage.getStyleClass().add("underline");
+		expenses.getStyleClass().add("underline");
+		areaLabel.getStyleClass().add("underline");
+		hours.getStyleClass().add("underline");	
+		monthlyExpenseText.getStyleClass().add("underline");	
+		areaOutputText.getStyleClass().add("underline");
+		oneBRAppPriceText.getStyleClass().add("underline");
+		wageTextLabel.getStyleClass().add("underline");
+		twoBRAppPriceText.getStyleClass().add("underline");	
+		housePriceText.getStyleClass().add("underline");	
+		
+		
+	
+		grid.getChildren().addAll(hourlyWage, oneBRAppPriceText, close, areaList, twoBRAppPriceText, housePriceText, oneBRAppOutput, 
+				twoBRAppOutput, housePriceOutput,netWageOutput, wageTextLabel, 
+				netWageText, expenses, areaLabel, hours, requiredInput, monthlyExpenseText, 
+				areaOutputText, areaOutputLabel);
+		
+		return grid;
+	}
+	
+	
+	
 }
-
 
 
 
--- HousingPrices.java
+++ HousingPrices.java
@@ -6,29 +6,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class HousingPrices {
 
+//class 
+public class HousingPrices {
+	
+//calling file	
 String pricesFile = "Housing_Prices_Table.txt";
  
 
 
+//Constructor
 HousingPrices(){
 
 }
 
+
+//Method to read file into a 2DArray
 public static String[][] ReadFileInto2DArray(String filepath, int numFields){
 	
+	//new arraylist
 	 List<String> pricesList = new ArrayList<>();
 	 
+	 //set delimiter
 	 String delimiter = "-";
 	 String currentLine;
 	 
 	 try {
 		 
-		 FileReader fr = new FileReader(filepath);
+		 //new Filereader and bufferedreader
+		FileReader fr = new FileReader(filepath);
 		 @SuppressWarnings("resource")
 		BufferedReader br = new BufferedReader(fr);
 		 
+		 //while loop to continue until file done
 		 while((currentLine = br.readLine()) != null)
 		 {
 			 pricesList.add(currentLine);
@@ -36,9 +46,11 @@ public static String[][] ReadFileInto2DArray(String filepath, int numFields){
 		 
 		 int pricesListCount = pricesList.size();
 		 
+		 //creating 2D array to return
 		 String arrayToReturn[][] = new String[pricesListCount][numFields];
 		 String[] data;
 		 
+		 //nested for loops to work through file and set values in appropriate array positions
 		 for(int i = 0; i < pricesListCount; i++) {
 			 
 			 data = pricesList.get(i).split(delimiter);
@@ -49,23 +61,30 @@ public static String[][] ReadFileInto2DArray(String filepath, int numFields){
 			}
 		 }	 
 		 
+		 //return 2D array
 		 return arrayToReturn;
 		 
-		 
+	//catch statement for potential null	 
 	} catch (Exception e) {
 		System.out.println(e);
 	    return null;
 	}
 }
 
+
+//creating 2D array and reading file into it
 String[][] housingPrices = ReadFileInto2DArray(pricesFile, 4);
 
-public String get1BrAppt(int cityNum) {
+
+//***********************Methods for getting values based on associated index************************//
+
+//method to return value in index 1 of any array in 2D array
+public String get1BrAppt(int areaNum) {
 	
 	
-	int i = cityNum;
+	int i = areaNum;
 	
-	
+		//switch statement to check which array in 2D array is needed and return value
 		switch (i){
 		case 0: return housingPrices[i][1];
 		case 1: return housingPrices[i][1];
@@ -88,10 +107,13 @@ public String get1BrAppt(int cityNum) {
 	}
 
 
+//method to return value in index 2 of any array in 2D array
 
-public String get2BrAppt(int cityNum) {
-	int i = cityNum;
+public String get2BrAppt(int areaNum) {
 	
+	int i = areaNum;
+	
+	//switch statement to check which array in 2D array is needed and return value
 	switch (i){
 	case 0: return housingPrices[i][2];
 	case 1: return housingPrices[i][2];
@@ -114,9 +136,12 @@ public String get2BrAppt(int cityNum) {
 
 	}
 
-
-public String getHouse(int cityNum) {
-	int i = cityNum;
+//method to return value in index 3 of any array in 2D array
+public String getHouse(int areaNum) {
+	
+	int i = areaNum;
+	
+	//switch statement to check which array in 2D array is needed and return value
 	switch (i){
 	case 0: return housingPrices[i][3];
 	case 1: return housingPrices[i][3];
@@ -137,5 +162,54 @@ public String getHouse(int cityNum) {
 	default: return "Location not chosen";
 		}
 	}
+
+
+//**************************Methods for determining affordability***********************//
+
+//method to generate message to user if 1 br appt is affordable based on net income
+public String affordability1BR(int areaNum, String netWageOutput) {
+
+			//get double values of net income and apartment price
+			double netIncome = Double.parseDouble(netWageOutput);
+			double oneBRPrice = Double.parseDouble(get1BrAppt(areaNum));
+			
+			//set limits to determine ranges
+			double cannotAfford = oneBRPrice - 100;
+			double canAfford = oneBRPrice + 100;
+	
+			//if else statements to return messages to user about if housing is affordable
+			if (netIncome < cannotAfford)
+				return "You cannot afford this apartment";
+				
+			else if (netIncome >= cannotAfford && netIncome <= canAfford)
+				return "You may be able to afford this apartment";
+			
+			else 
+				return "You can afford this apartment";
+			
+}
+
+//method to generate message to user if 2 br appt is affordable based on net income
+public String affordability2BR(int areaNum, String netWageOutput) {
+	
+	//get double values of net income and apartment price
+	double netIncome = Double.parseDouble(netWageOutput);
+	double twoBRPrice = Double.parseDouble(get2BrAppt(areaNum));
+	
+	//set limits to determine ranges
+	double cannotAfford = twoBRPrice - 100;
+	double canAfford = twoBRPrice + 100;
+
+	//if else statements to return messages to user about if housing is affordable
+	if (netIncome < cannotAfford)
+		return "You cannot afford this apartment";
+		
+	else if (netIncome >= cannotAfford && netIncome <= canAfford)
+		return "You may be able to afford this apartment";
+	
+	else 
+		return "You can afford this apartment";
+	
+}
 
 }
